40d62e9fc1b19dcc8483472a8be7c8f3ae98a11b
Tewodros Wondimu <tewodros.wr@gmail.com>
2014-02-26 12:47:23 +0300

    Second Commit

---
+++ app/Controller/PostsController.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * Created on: Feb 25, 2014
+ *  File Name: PostsController.php
+ *  Developer: Tewodros Wondimu Robi
+ *  Copyright: 2014
+ */
+ 
+ class PostsController extends AppController {
+ 	public $helpers = array('Html', 'Form');
+ 	
+ 	public function index() {
+ 		$this->set('posts', $this->Post->find('all'));
+ 	}
+ 	
+ 	public function view($id = null) {
+ 		if (!id) {
+ 			throw new NotFoundException(_('Invalid post'));
+ 		}
+ 		
+ 		$post = $this->Post->findById($id);
+ 		if (!$post) {
+ 			throw new NotFoundException(_('Invalid post'));
+ 		}
+ 		$this->set('post', $post);
+ 	}
+ }
+?>
---
+++ app/Model/Post.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * Created on Feb 25, 2014
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ */
+ 
+ class Post extends AppModel {
+ 	
+ }
+ 
+?>

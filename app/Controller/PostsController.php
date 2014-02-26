9e516478eb5ac72d00a4a817935c5dd364f433e6
Tewodros Wondimu <tewodros.wr@gmail.com>
2014-02-26 18:22:08 +0300

    Added new files

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -7,7 +7,7 @@
  */
 
  class PostsController extends AppController {
- 	public $helpers = array('Html', 'Form');
+ 	public $helpers = array('Html', 'Form', 'Session');
 
  	public function index() {
  		$this->set('posts', $this->Post->find('all'));
@@ -23,6 +23,55 @@
  			throw new NotFoundException(_('Invalid post'));
  		}
  		$this->set('post', $post);
+ 	}
+
+ 	public function add() {
+ 		if ($this->request->is('post')) {
+ 			$this->Post->create();
+ 			if ($this->Post->save($this->request->data)) {
+ 				$this->Session->setFlash(_('Your post has been saved.'));
+ 				return $this->redirect(array('action' => 'index'));
+ 			}
+ 			$this->Session->setFlash(_('Unable to add your post.'));
+ 		}
+ 	}
+
+ 	public function edit($id = null) {
+ 		if (!$id) {
+ 			throw new NotFoundException(_('Invalid Post'));
+ 		}
+
+ 		$post = $this->Post->findById($id);
+ 		if (!$post) {
+ 			throw new NotFoundException(_('Invalid Post'));
+ 		}
+
+ 		if($this->request->is(array('post', 'put'))) {
+ 			$this->Post->id = $id;
+ 			if ($this->Post->save($this->request->data)) {
+ 				$this->Session->setFlash(_('Your post has been updated'));
+ 				return $this->redirect(array('action' => 'index'));
+ 			}
+ 			$this->Session->setFlash(_('Unable to update your post.'));
+ 		}
+
+ 		if (!$this->request->data) {
+ 			$this->request->data = $post;
+ 		}
+ 	}
+
+ 	public function delete($id) {
+ 		if ($this->request->is('get')) {
+ 			throw new MethodNotAllowedException();
+ 		}
+
+ 		if ($this->Post->delete($id)) {
+ 			$this->Session->setFlash(
+            	_('The post has been deleted.')
+        	);
+ 			return $this->redirect(array('action' => 'index'));
+ 		}
+ 		$this->Session->setFlash(_('Unable to delete post :('));
  	}
  }
 ?>
--- app/Model/Post.php
+++ app/Model/Post.php
@@ -5,9 +5,16 @@
  * To change the template for this generated file go to
  * Window - Preferences - PHPeclipse - PHP - Code Templates
  */
- 
+
  class Post extends AppModel {
- 	
+ 	public $validate = array (
+ 		'title' => array(
+ 			'rule' => 'notEmpty'
+ 		),
+ 		'body' => array (
+ 			'rule' => 'notEmpty'
+ 		)
+ 	);
  }
- 
+
 ?>

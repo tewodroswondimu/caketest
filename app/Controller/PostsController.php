e2b0ca99831536633dd150f0ffcbc005c669d6a5
Tewodros Wondimu <tewodros.wr@gmail.com>
2014-02-26 13:24:19 +0300

    Error raised with simple use of id instead of $id

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -5,19 +5,19 @@
  *  Developer: Tewodros Wondimu Robi
  *  Copyright: 2014
  */
- 
+
  class PostsController extends AppController {
  	public $helpers = array('Html', 'Form');
- 	
+
  	public function index() {
  		$this->set('posts', $this->Post->find('all'));
  	}
- 	
+
  	public function view($id = null) {
- 		if (!id) {
+ 		if (!$id) {
  			throw new NotFoundException(_('Invalid post'));
  		}
- 		
+
  		$post = $this->Post->findById($id);
  		if (!$post) {
  			throw new NotFoundException(_('Invalid post'));
